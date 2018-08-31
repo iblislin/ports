46fd44671b1d74f39da48d68beebcee3a5150154
Iblis Lin <iblis@hs.ntnu.edu.tw>
2018-08-31 11:42:29 +0800

    net/mosquitto: bump to 1.5.1

--- net/mosquitto/files/patch-lib_mosquitto__internal.h
+++ net/mosquitto/files/patch-lib_mosquitto__internal.h
@@ -1,6 +1,6 @@
---- lib/mosquitto_internal.h.orig	2017-07-10 22:46:01 UTC
+--- lib/mosquitto_internal.h.orig	2018-08-16 16:05:26 UTC
 +++ lib/mosquitto_internal.h
-@@ -30,11 +30,7 @@ Contributors:
+@@ -31,11 +31,7 @@ Contributors:
  #endif
  #include <stdlib.h>
  
@@ -13,7 +13,7 @@
  
  #ifdef WITH_SRV
  #  include <ares.h>
-@@ -190,7 +186,7 @@ struct mosquitto {
+@@ -194,7 +190,7 @@ struct mosquitto {
  #endif
  	bool want_write;
  	bool want_connect;
@@ -22,7 +22,7 @@
  	pthread_mutex_t callback_mutex;
  	pthread_mutex_t log_callback_mutex;
  	pthread_mutex_t msgtime_mutex;
-@@ -201,7 +197,7 @@ struct mosquitto {
+@@ -205,7 +201,7 @@ struct mosquitto {
  	pthread_mutex_t out_message_mutex;
  	pthread_mutex_t mid_mutex;
  	pthread_t thread_id;
